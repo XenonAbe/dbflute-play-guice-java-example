bbd586a274c8dc2c19887fedf498a810c78695c5
abe <xenon.abe@gmail.com>
2017-10-21 17:37:07 +0900

    パッケージ依存定義修正 H2 はデフォルトのバージョンを使用

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,17 +1,9 @@
 import sbt._
-import sbt.Keys._
-import Keys._
-import com.typesafe.sbteclipse.core._
-import com.typesafe.sbteclipse.plugin.EclipsePlugin._
-import com.typesafe.sbteclipse.core.Validation
-import scala.xml._
-import scala.xml.transform.RewriteRule
 
 object Dependencies {
 
   // see framework/project/Dependencies.scala in Play Framework Source
   val springframeworkVersion = "4.0.3.RELEASE"
-  val h2databaseVersion = "1.3.175"
 
   val guiceVersion = "3.0"
   val guice = "com.google.inject" % "guice" % Dependencies.guiceVersion
