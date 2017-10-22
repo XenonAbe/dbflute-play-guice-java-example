d38b3dde62a20e843787182357a9c7cb7728c4bb
abe <xenon.abe@gmail.com>
2017-10-22 10:44:52 +0900

    DBFlute Client 1.0.5N -> 1.1.4

--- dbflute/dbflute_exampledb/_project.bat
+++ dbflute/dbflute_exampledb/_project.bat
@@ -2,9 +2,7 @@
 
 set ANT_OPTS=-Xmx512m
 
-set DBFLUTE_HOME=..\mydbflute\dbflute-1.0.5N
-
-set MY_PROJECT_NAME=dfclient
+set DBFLUTE_HOME=..\mydbflute\dbflute-1.1.4
 
 set MY_PROPERTIES_PATH=build.properties
 
--- dbflute/dbflute_exampledb/_project.sh
+++ dbflute/dbflute_exampledb/_project.sh
@@ -2,8 +2,10 @@
 
 export ANT_OPTS=-Xmx512m
 
-export DBFLUTE_HOME=../mydbflute/dbflute-1.0.5N
-
-export MY_PROJECT_NAME=dfclient
+export DBFLUTE_HOME=../mydbflute/dbflute-1.1.4
 
 export MY_PROPERTIES_PATH=build.properties
+
+if [ `uname` = "Darwin" ]; then
+  export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+fi
