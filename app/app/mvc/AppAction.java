11b3daa52834f9bc3653be0445940e74c374c57a
abe <xenon.abe@gmail.com>
2017-10-24 02:53:24 +0900

    AppActionに非同期実行を考慮した実装を追加

--- app/app/mvc/AppAction.java
+++ app/app/mvc/AppAction.java
@@ -10,6 +10,7 @@ import play.mvc.Result;
 
 import java.lang.reflect.Method;
 import java.time.LocalDateTime;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 
 public class AppAction extends Action.Simple {
@@ -25,24 +26,84 @@ public class AppAction extends Action.Simple {
 
     @Override
     public CompletionStage<Result> call(Http.Context ctx) {
+        CompletionStage<Result> resultCompletionStage;
         try {
+            onBegin(ctx);
             try {
-                initializeContext(ctx);
-                return delegate.call(ctx);
-            } finally {
-                cleanupContext(ctx);
+                resultCompletionStage = delegate.call(ctx);
+                if (logger.isInfoEnabled() && resultCompletionStage instanceof CompletableFuture && !((CompletableFuture)resultCompletionStage).isDone())
+                    logger.info("ASYNC ctx:{}", ctx.id());
+            } catch(Throwable t) {
+                resultCompletionStage = CompletableFuture.completedFuture(handleException(ctx, t));
             }
-        } catch(RuntimeException e) {
-            throw e;
         } catch(Throwable t) {
-            throw new RuntimeException(t);
+            onComplete(ctx, null, t);
+            throw t;
         }
+
+        resultCompletionStage = resultCompletionStage.exceptionally(throwable -> {
+            final Http.Context oldCtx = Http.Context.current.get();
+            if (oldCtx == null) Http.Context.current.set(ctx);
+            try {
+                return handleException(Http.Context.current(), throwable);
+            } finally {
+                Http.Context.current.set(oldCtx);
+            }
+        });
+
+        resultCompletionStage = resultCompletionStage.whenComplete((result, throwable) -> {
+            final Http.Context oldCtx = Http.Context.current.get();
+            if (oldCtx == null) Http.Context.current.set(ctx);
+            try {
+                onComplete(Http.Context.current(), result, throwable);
+            } finally {
+                Http.Context.current.set(oldCtx);
+            }
+        });
+
+        return resultCompletionStage;
     }
 
-    protected void initializeContext(Http.Context ctx) {
+    protected Result handleException(Http.Context ctx, Throwable throwable) {
+        if (logger.isWarnEnabled())
+            logger.warn("Exception Occurred ctx:{} {}: {}", ctx.id(), throwable.getClass().getSimpleName(), throwable.getMessage());
+
+        if (throwable instanceof RuntimeException)
+            throw (RuntimeException)throwable;
+        else if (throwable instanceof Error)
+            throw (Error)throwable;
+        throw new RuntimeException(throwable);
+    }
+
+    protected void onBegin(Http.Context ctx) {
         if (logger.isInfoEnabled())
             logger.info("BEGIN ctx:{} {} {}", ctx.id(), ctx.request().method(), ctx.request().uri());
 
+        initializeContext(ctx);
+    }
+
+    protected void onComplete(Http.Context ctx, Result result, Throwable throwable) {
+        try {
+            if (throwable != null)
+                onFailure(Http.Context.current(), throwable);
+            else
+                onDone(Http.Context.current(), result);
+        } finally {
+            cleanupContext(ctx);
+        }
+    }
+
+    protected void onDone(Http.Context ctx, Result result) {
+        if (logger.isInfoEnabled())
+            logger.info("DONE  ctx:{} {}", ctx.id(), result.status());
+    }
+
+    protected void onFailure(Http.Context ctx, Throwable throwable) {
+        if (logger.isErrorEnabled())
+            logger.error("FAIL  ctx:{}", ctx.id(), throwable);
+    }
+
+    protected void initializeContext(Http.Context ctx) {
         // DBFlute AccessContextの設定
         AccessContext.setAccessContextOnThread(createAccessContext());
     }
@@ -51,9 +112,6 @@ public class AppAction extends Action.Simple {
         for (String key : Constants.CtxArgsKeys) {
             ctx.args.remove(key);
         }
-
-        if (logger.isInfoEnabled())
-            logger.info("END   ctx:{}", ctx.id());
     }
 
     protected AccessContext createAccessContext() {
